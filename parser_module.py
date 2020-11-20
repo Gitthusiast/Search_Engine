9c42dc5f75a657fca720994723a7c3408a4a8993
Gitthusiast <dorfmag@post.bgu.ac.il>
2020-11-20 22:11:03 +0200

    fixed parsing of hashtag

--- parser_module.py
+++ parser_module.py
@@ -8,66 +8,6 @@ class Parse:
     def __init__(self):
         self.stop_words = stopwords.words('english')
 
-    def parse_hashtag2(self, text_tokens, i):
-        """
-        This function parses the hashtag including tokens, according to the rules.
-        :param text_tokens: tokens from the text
-        :return:
-        """
-        if len(text_tokens) > i + 1:
-            hashtag = text_tokens[i + 1]
-            text_tokens.remove(text_tokens[i + 1])
-            from_index = 0  # indicating where to slice the string from
-            joined_hashtag = ""
-            insertion_index = 0  # indication where to insert the new token
-            j = 0  # iterator for the hashtag string
-            while j < len(hashtag):
-                if hashtag[j].isupper() or j == len(hashtag) - 1 or hashtag[j] in ['.', '…', ',', '_', '-', '.']:
-                    if j == len(hashtag) - 1:
-                        if hashtag[j] not in ['.', '…', ',', '_', '-', '.']:
-                            text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j + 1].lower())
-                            joined_hashtag += hashtag[from_index + 1:j + 1].lower()
-                            insertion_index += 1
-                    elif hashtag[j].isupper():
-                        if len(hashtag) > j+1 and hashtag[j+1].isupper():  # interpret word as acronym
-                            if hashtag[from_index:j] != '':
-                                text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j].lower())
-                                from_index = j
-                                insertion_index += 1
-                            while j < len(hashtag):
-                                if len(hashtag) > j + 1 and not hashtag[j+1].isupper():
-                                    if from_index == 0:
-                                        text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j + 1].lower())
-                                        joined_hashtag += hashtag[from_index:j + 1].lower()
-                                    else:
-                                        text_tokens.insert(i + 1 + insertion_index, hashtag[from_index + 1:j + 1].lower())
-                                        joined_hashtag += hashtag[from_index + 1:j + 1].lower()
-                                    from_index = j + 1
-                                    insertion_index += 1
-                                    break
-                                j += 1
-                            if j == len(hashtag):
-                                text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j].lower())
-                                joined_hashtag += hashtag[from_index:j].lower()
-                                insertion_index += 1
-                                break
-                        elif j != 0:
-                            text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j].lower())
-                            from_index = j
-                            insertion_index += 1
-                    elif hashtag[j] in ['.', '…', ',', '_', '-', '.']:
-                        if hashtag[from_index:j] != '':
-                            text_tokens.insert(i + 1 + insertion_index, hashtag[from_index:j].lower())
-                            from_index = j + 1
-                            insertion_index += 1
-
-                elif j < len(hashtag) and not hashtag[j] in ['.', '…', ',', '_', '-', '.', '']:
-                    joined_hashtag += hashtag[j].lower()
-
-                j += 1
-
-            text_tokens[i] += joined_hashtag
-
     def parse_hashtag_underscore(self, text_tokens, i):
         token = text_tokens[i+1]
         text_tokens.remove(text_tokens[i+1])
@@ -104,40 +44,6 @@ class Parse:
             text_tokens.insert(i + 1 + insertion_index, token[from_index:len(token)].lower())
         text_tokens[i] = joined_hashtag
 
-    def parse_hashtag_capital(self, text_tokens, i):
-
-        token = text_tokens[i + 1]
-        text_tokens.remove(text_tokens[i + 1])
-        j = 0
-        joined_hashtag = '#'
-        from_index = 0
-        insertion_index = 0
-        while j < len(token):
-            if j + 1 < len(token) and token[j].isupper() and token[j+1].isupper():
-                if token[from_index:j] != '':
-                    text_tokens.insert(i + 1 + insertion_index, token[from_index:j].lower())
-                    from_index = j
-                    insertion_index += 1
-                while j < len(token) and token[j].isupper():
-                    j += 1
-                text_tokens.insert(i + 1 + insertion_index, token[from_index:j])
-                joined_hashtag += token[from_index:j].lower()
-                from_index = j
-                insertion_index += 1
-                j -= 1
-            elif token[j].isupper() and j != 0:
-                if token[from_index:j] != '':
-                    text_tokens.insert(i + 1 + insertion_index, token[from_index:j].lower())
-                    from_index = j
-                    insertion_index += 1
-                joined_hashtag += token[j].lower()
-            else:  # lower case letters
-                joined_hashtag += token[j].lower()
-            j += 1
-        if token[from_index:len(token)] != '':
-            text_tokens.insert(i + 1 + insertion_index, token[from_index:j].lower())
-        text_tokens[i] = joined_hashtag
-
     def parse_hashtag(self, text_tokens, i):
 
         # parsing snake case
@@ -147,13 +53,13 @@ class Parse:
         # parsing pascal and camel cases
         camel_case = True
         if len(text_tokens) > i + 1:
-            for j in range(len(text_tokens[i+1])-1):
-                if text_tokens[i+1][j].isupper() and text_tokens[i+1][j+1].isupper():
-                    camel_case = False  # not camel case - word of type "CULTforGOOD" different parsing
+            # for j in range(len(text_tokens[i+1])-1):
+            #     if text_tokens[i+1][j].isupper() and text_tokens[i+1][j+1].isupper():
+            #         camel_case = False  # not camel case - word of type "CULTforGOOD" different parsing
             if camel_case:
                 self.parse_hashtag_camel_case(text_tokens, i)
-            else:
-                self.parse_hashtag_capital(text_tokens, i)  # parsing words with capital letters - acronyms
+            # else:
+            #     self.parse_hashtag_capital(text_tokens, i)  # parsing words with capital letters - acronyms
 
     def parse_tagging(self, text_tokens, i):
 
@@ -182,14 +88,11 @@ class Parse:
 
     def parse_sentence(self, text):
 
-        # text = "6% 10.6 percent 11 percentage 7 % %7 11.1%"
         """
         This function tokenize, remove stop words and apply lower case for every word within the text
         :param text:
         :return:
         """
-        text = "#stay_at_home #StayAtHome #stayAtHome"
-        print(text)
         text_tokens = word_tokenize(text)
 
         for index, token in enumerate(text_tokens):
@@ -200,17 +103,7 @@ class Parse:
             if token == 'https' or token == 'http':
                 self.parse_url(text_tokens, index)
 
-        """
-        print(str(text_tokens.count(':')) + "\n")
-        if text_tokens.count(':') != 0:
-            print(text_tokens[text_tokens.index(':') - 1])
-        """
-
         print(text_tokens)
-        print("\n\n")
-
-
-
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
